42316914c0755b9822965d82cc373b49d945780d
Josbell Quiros <josbell.quiros@oasisdigital.com>
2021-01-25 11:19:39 -0500

    Add Tests

--- functions/src/tests/convert-handler.spec.ts
+++ functions/src/tests/convert-handler.spec.ts
@@ -1,9 +1,10 @@
 import convertHandler from '../convert-handler';
 import { assert } from 'chai';
 import * as functions from 'firebase-functions';
+import { GetConvertedValueRequest } from '../model';
 
 describe('Convert Handler', () => {
-  it('should return ', () => {
+  it('should return success object when inputs are correct', () => {
     const data = {
       startingValue: 5,
       startingUnit: 'lb',
@@ -12,6 +13,30 @@ describe('Convert Handler', () => {
     const context = {} as functions.https.CallableContext;
     return convertHandler(data, context).then((val) => {
       assert.deepEqual(val, { status: 'success', convertedValue: 80 });
+    });
+  });
+
+  it('should return error object when called with missing arguments', () => {
+    const data = ({
+      startingValue: null,
+      startingUnit: null,
+      convertedUnit: 'oz',
+    } as any) as GetConvertedValueRequest;
+    const context = {} as functions.https.CallableContext;
+    return convertHandler(data, context).then((val) => {
+      assert.deepEqual(val.status, 'error');
+    });
+  });
+
+  it('should return error object when called with invalid arguments', () => {
+    const data = ({
+      startingValue: 6,
+      startingUnit: 'K',
+      convertedUnit: 'oz',
+    } as any) as GetConvertedValueRequest;
+    const context = {} as functions.https.CallableContext;
+    return convertHandler(data, context).then((val) => {
+      assert.deepEqual(val.status, 'error');
     });
   });
 });
